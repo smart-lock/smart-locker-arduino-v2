854c5a1cbae925cf9ff39719be61c1cf3a6820f4
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-19 16:00:43 -0300

    DoorSensor

--- lib/DoorSensor/DoorSensor.cpp
+++ lib/DoorSensor/DoorSensor.cpp
@@ -1,13 +1,28 @@
 #include <DoorSensor.h>
+#include <DebouncedPin.h>
+#include "Arduino.h"
 
-DoorSensor::DoorSensor() {
+// void handleSwitchChange (int value, bool firstCall) {
+//     if (value == HIGH) {
+//       onOpen();
+//     } else {
+//       onClose();
+//     }
+//   }
 
+DoorSensor::DoorSensor(int switchPin, void (*onOpen)(void), void (*onClose)(void) ) {
+  _switchPin = switchPin;
+  unsigned long delay = 500;
+
+  DebouncedPin _debouncedPin(switchPin, delay, [](int value, bool firstCall) {
+    
+  });
 }
 
+void DoorSensor::loop() {
+    
+};
+
 void DoorSensor::setup() {
-
-}
-
-void DoorSensor::setup() {
-
+  
 }
--- lib/DoorSensor/DoorSensor.h
+++ lib/DoorSensor/DoorSensor.h
@@ -1,12 +1,17 @@
 #ifndef DoorSensor_h
 #define DoorSensor_h
 #include <SystemComponent.h>
+#include <DebouncedPin.h>
 
 class DoorSensor: public ISystemComponent {
   public:
-    DoorSensor();
-    void loop();
-    void setup();
+    DoorSensor(int switchPin, void (*onOpenDoor)(void), void (*onClose)(void) );
+    virtual void loop();
+    virtual void setup();
+
+    bool closed;
+  private:
+    int _switchPin;
 };
 
 #endif
