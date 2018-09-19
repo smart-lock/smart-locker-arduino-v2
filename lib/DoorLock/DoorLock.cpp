89677ee1737c8ff30ae73b48cdd5e2919ec3f0bb
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-19 16:00:37 -0300

    DoorLock

--- lib/DoorLock/DoorLock.cpp
+++ lib/DoorLock/DoorLock.cpp
@@ -1,13 +1,27 @@
 #include <DoorLock.h>
+#include <Servo.h>
 
-DoorLock::DoorLock() {
+const int SERVO_LOCK_ANGLE = 180;
+const int SERVO_UNLOCK_ANGLE = 0;
 
+DoorLock::DoorLock(int servoPin) {
+  _servoPin;
 }
 
-void DoorLock::setup() {
+void DoorLock::loop() {
+    
+};
 
+void DoorLock::setup() {
+  _lockServo.attach(_servoPin);
 }
 
-void DoorLock::setup() {
+void DoorLock::lock() {
+  _locked = true;
+  _lockServo.write(SERVO_LOCK_ANGLE);
+}
 
+void DoorLock::unlock() {
+  _locked = false;
+  _lockServo.write(SERVO_UNLOCK_ANGLE);
 }
--- lib/DoorLock/DoorLock.h
+++ lib/DoorLock/DoorLock.h
@@ -1,12 +1,19 @@
 #ifndef DoorLock_h
 #define DoorLock_h
 #include <SystemComponent.h>
+#include <Servo.h>
 
 class DoorLock: public ISystemComponent {
   public:
-    DoorLock();
-    void loop();
-    void setup();
+    DoorLock(int servoPin);
+    virtual void loop();
+    virtual void setup();
+    void lock();
+    void unlock();
+  private:
+    bool _locked;
+    Servo _lockServo;
+    int _servoPin;
 };
 
 #endif
