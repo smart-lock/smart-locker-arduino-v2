15828fd5e10468c5a45121d10bdbad2cd510a854
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-24 02:39:05 -0300

    View.h camera

--- lib/View/View.h
+++ lib/View/View.h
@@ -3,11 +3,17 @@
 #include <LockerCluster.h>
 class View {
   public:
-    View();
+    View(LockerCluster *lockerCluster);
     int16_t lcdScreenWidth;
     int16_t lcdScreenHeight;
+    uint8_t cameraX = 0;
+    uint8_t cameraY = 0;
     void setup();
+    void drawLockerCluster();
+    void setCameraX(int x);
+    void setCameraY(int y);
+  private:
+    LockerCluster *_lockerCluster;
 };
-void drawLockerCluster(LockerCluster *lockerCluster, uint32_t x, uint32_t y, uint32_t w, uint32_t h);
 
 #endif
